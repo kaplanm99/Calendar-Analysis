d7590820046097e9032f86b5da69633f554ddfa4
Michael Kaplan <kaplanm99@gmail.com>
2013-04-13 23:04:18 -0400

    Change chart shown using form select elements.

--- getChartDataJSON.php
+++ getChartDataJSON.php
@@ -1,63 +1,77 @@
 <?php
 
-    $data = array();
+    if(isset($_GET["data_analysis_type"]) && isset($_GET["nonrecurring_included"]) && isset($_GET["recurring_included"]) ) {    
+        
+        $data_analysis_type = strip_tags($_GET["data_analysis_type"]);
+        $data_analysis_type = trim($data_analysis_type);
+        $data_analysis_type = filter_var($data_analysis_type, FILTER_SANITIZE_STRING);
+        
+        $nonrecurring_included = strip_tags($_GET["nonrecurring_included"]);
+        $nonrecurring_included = trim($nonrecurring_included);
+        $nonrecurring_included = intval($nonrecurring_included);
 
-    require('db/config.php');
-    $mysqli = new mysqli($host, $username, $password, $db);
-    if ($stmt = $mysqli->prepare("SELECT `user_id`, `array_serialized` FROM `data_analysis` WHERE `data_analysis_type` = ? AND `nonrecurring_included` = ? AND `recurring_included` = ? ;")){
+        $recurring_included = strip_tags($_GET["recurring_included"]);
+        $recurring_included = trim($recurring_included);
+        $recurring_included = intval($recurring_included);
+
+        $data = array();
+
+        require('db/config.php');
+        $mysqli = new mysqli($host, $username, $password, $db);
+        if ($stmt = $mysqli->prepare("SELECT `user_id`, `array_serialized` FROM `data_analysis` WHERE `data_analysis_type` = ? AND `nonrecurring_included` = ? AND `recurring_included` = ? ;")){
+            
+            //$data_analysis_type = "day_of_the_week_created";
+            
+            //$nonrecurring_included = 1;
+            //$recurring_included = 0;
+            
+            $stmt->bind_param('sii', $data_analysis_type, $nonrecurring_included, $recurring_included);
+            $stmt->execute();
+            $stmt->bind_result($user_id, $array_serialized);
+            
+            while($stmt->fetch()) {
+                $data[$user_id] = unserialize($array_serialized);
+            }
+            
+            $stmt->close();
+
+        }
+           
+        $mysqli->close();
         
-        $data_analysis_type = "day_of_the_week_created";
         
-        $nonrecurring_included = 1;
-        $recurring_included = 0;
         
-        $stmt->bind_param('sii', $data_analysis_type, $nonrecurring_included, $recurring_included);
-        $stmt->execute();
-        $stmt->bind_result($user_id, $array_serialized);
         
-        while($stmt->fetch()) {
-            $data[$user_id] = unserialize($array_serialized);
+        print('{ "cols": [ {"id":"","label":"day_of_the_week_created","pattern":"","type":"string"},');
+        
+        foreach($data as $user_id => $array_unserialized) {        
+            print('{"id":"","label":"'.$user_id.'","pattern":"","type":"number"},');
         }
         
-        $stmt->close();
-
-    }
-       
-    $mysqli->close();
-    
-    
-    
-    
-    print('{ "cols": [ {"id":"","label":"day_of_the_week_created","pattern":"","type":"string"},');
-    
-    foreach($data as $user_id => $array_unserialized) {        
-        print('{"id":"","label":"'.$user_id.'","pattern":"","type":"number"},');
-    }
-    
-    print(' ], "rows": [ ');
-    
-    $daysOfTheWeek = array();
-    $daysOfTheWeek[1] = "Monday";
-    $daysOfTheWeek[2] = "Tuesday";
-    $daysOfTheWeek[3] = "Wednesday";
-    $daysOfTheWeek[4] = "Thursday";
-    $daysOfTheWeek[5] = "Friday";
-    $daysOfTheWeek[6] = "Saturday";
-    $daysOfTheWeek[7] = "Sunday";
-    
-    for($i = 1;$i <= 7;$i++) {
+        print(' ], "rows": [ ');
         
-        print('{"c":[{"v":"'.$daysOfTheWeek[$i].'"},');
+        $daysOfTheWeek = array();
+        $daysOfTheWeek[1] = "Monday";
+        $daysOfTheWeek[2] = "Tuesday";
+        $daysOfTheWeek[3] = "Wednesday";
+        $daysOfTheWeek[4] = "Thursday";
+        $daysOfTheWeek[5] = "Friday";
+        $daysOfTheWeek[6] = "Saturday";
+        $daysOfTheWeek[7] = "Sunday";
         
-        foreach($data as $user_id => $array_unserialized) {            
-            print('{"v":'.$array_unserialized[$i].'},');
+        for($i = 1;$i <= 7;$i++) {
+            
+            print('{"c":[{"v":"'.$daysOfTheWeek[$i].'"},');
+            
+            foreach($data as $user_id => $array_unserialized) {            
+                print('{"v":'.floatval($array_unserialized[$i]).'},');
+            }
+            
+            print(']},');
+            
         }
         
-        print(']},');
-        
+        print(']}');
     }
-    
-    print(']}');
-    
         
 ?>
--- googleLineChart.php
+++ googleLineChart.php
@@ -5,31 +5,61 @@
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
     <script type="text/javascript">
     
+    var data_analysis_type = "data_analysis_type=day_of_the_week_created&";
+    var event_type_included = "nonrecurring_included=1&recurring_included=0&";
+    
     // Load the Visualization API and the piechart package.
     google.load('visualization', '1', {'packages':['corechart']});
-      
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
-      
-    function drawChart() {
-      var jsonData = $.ajax({
-          url: "getChartDataJSON.php?data_analysis_type=day_of_the_week_created&nonrecurring_included=1&recurring_included=0",
-          dataType:"json",
-          async: false
-          }).responseText;
+    
+    $(document).ready(function(){
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.setOnLoadCallback(drawChart);
+        
+        function drawChart() {
+          var jsonData = $.ajax({
+              url: ("getChartDataJSON.php?"+data_analysis_type+event_type_included),
+              dataType:"json",
+              async: false
+              }).responseText;
+              
+          // Create our data table out of JSON data loaded from server.
+          var data = new google.visualization.DataTable(jsonData);
+
+          // Instantiate and draw our chart, passing in some options.
+          var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+          chart.draw(data, {width: 900, height: 400, vAxis: {title: "Number of Nonrecurring Events"}, hAxis: {title: "Day of the Week Created"} });
+        }
+        
+        $("#data_analysis_type").change(function() {
+          data_analysis_type = $("#data_analysis_type").val();
+          event_type_included = $("#event_type_included").val();
           
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
-
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, {width: 900, height: 400, vAxis: {title: "Number of Nonrecurring Events"}, hAxis: {title: "Day of the Week Created"} });
-    }
-
+          drawChart();
+        });
+        
+        $("#event_type_included").change(function() {
+          data_analysis_type = $("#data_analysis_type").val();
+          event_type_included = $("#event_type_included").val();
+          
+          drawChart();
+        });
+    });
     </script>
   </head>
 
   <body>
+    <form>
+        <select id="data_analysis_type">
+            <option value="data_analysis_type=day_of_the_week_created&">day_of_the_week_created</option>
+        </select>
+
+        <select id="event_type_included">
+            <option value="nonrecurring_included=1&recurring_included=0&">Nonrecurring Events</option>
+            <option value="nonrecurring_included=0&recurring_included=1&">Recurring Events</option>
+            <option value="nonrecurring_included=1&recurring_included=1&">Nonrecurring and Recurring Events</option>
+        </select> 
+    </form>
+    
     <!--Div that will hold the pie chart-->
     <div id="chart_div"></div>
   </body>
